73577bb01c8ef2b83f8ebb41f5a9a98fab8f2b6c
Thukera <31145663+Thukera@users.noreply.github.com>
2020-09-22 17:54:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ try {
 }
 
 function handleElements() {
-    var text = document.getElementsById('#transcript_bubble_agent_text').Value;
+    var text = document.querySelector('div.lpc_message__text_agent').innerHTML;
     alert(text);
     if(text.include("#")){
-      document.getElementsById('#transcript_bubble_agent_text').style.color = "#FF5733";
+      document.querySelector('div.lpc_message__text_agent').style.color = "#FF5733";
     }
   }
